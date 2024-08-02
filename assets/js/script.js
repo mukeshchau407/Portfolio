868136b8c4359c7a4131c8d5f310cb873cef6d04
Mukesh ChaudhaRy <mukesh@kali>
2024-08-02 17:12:13 +0545

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Front-End Developer" , "Web Developer" , "Back-End Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});

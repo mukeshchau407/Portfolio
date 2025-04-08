7e5be65b5884910087805ec773083b38021a9856
mukeshchau407 <chymukesh5@gmail.com>
2025-04-08 12:07:30 +0545

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var typed = new Typed(".text",{
+var typed = new typed(".text",{
     strings:["Front-End Developer" , "Web Developer" , "Back-End Developer"],
     typeSpeed:100,
     backSpeed:100,
@@ -9,4 +9,4 @@ var typed = new Typed(".text",{
 const $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e =>{
     $header.classList[window.scrollY > 50 ? "add" : "remove"] ("active");
-}); 
+});
